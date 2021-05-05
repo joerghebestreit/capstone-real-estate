33296efa107c89de66e27080be0b8a2b996a155f
joerghebestreit <joerghebestreit@gmail.com>
2021-05-05 18:07:43 +0200

    add Carousel

---
+++ client/src/components/Carousel.js
@@ -0,0 +1,36 @@
+import "../components/Carousel.css"
+import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from "react-icons/fa";
+import {useState} from "react";
+
+export default function Carousel({images}) {
+    const [picture, setPicture] = useState(0)
+    const length = images.length
+
+    const nextSlide = () => {
+        setPicture(picture === length - 1 ? 0 : picture + 1)
+    }
+
+    const prevSlide = () => {
+        setPicture(picture === 0 ? length -1 : picture - 1)
+    }
+
+    console.log(picture)
+
+    if(!Array.isArray(images) || images.length <= 0) {
+        return null
+    }
+
+    return(
+      <div className="carousel">
+          <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide}/>
+          <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide}/>
+        {images.map((images, index) => {
+          return(
+            <div className={index === picture ? "slideActive" : "slide"} key={index}>
+                {index === picture && (<img className="image" src={images}/>)} 
+            </div>
+          )
+        })}
+      </div>
+    );
+  }
--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -1,16 +1,13 @@
 import "../components/Details.css";
 import { useState, useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
+import Carousel from "../components/Carousel"
 
-
-function Carousel() {
-  
-}
 
 export default function Details() {
   const { ID } = useParams();
   const history = useHistory();
-  const [property, setProperty] = useState({});
+  const [property, setProperty] = useState();
  
     console.log("Hallo von Details")
     console.log(ID)
@@ -25,39 +22,44 @@ export default function Details() {
       });
   }, [ID]);
 
-
-  return (
-    <div className="detailDiv">
-      <div className="p-1">
-        <div className="p-1-1">
-          <p className="p-a">{property.City}</p>
-          <p className="p-a">{property.Street}</p>
-        </div>
-        <div className="p-1-2">
-          <p className="p-a">{property.Price}</p>
-          <p className="p-a">ID: {property.ID}</p>
+    return ( property ? 
+      <div>
+        <Carousel images={property.img}/>
+        <div className="detailDiv">
+          <div className="p-1">
+            <div className="p-1-1">
+              <p className="p-a">{property.City}</p>
+              <p className="p-a">{property.Street}</p>
+            </div>
+            <div className="p-1-2">
+              <p className="p-a">{property.Price}</p>
+              <p className="p-a">ID: {property.ID}</p>
+            </div>
+          </div>
+          <hr className="hr"></hr>
+          <div className="p-1">
+            <div className="p-2-1">
+              <p className="p-m1">Year Build</p>
+              <p className="p-m1">Interior</p>
+              <p className="p-m1">Bedrooms</p>
+              <p className="p-m1">Status</p>
+            </div>
+            <div className="p-2-2">
+              <p className="p-m2">{property.YearBuild}</p>
+              <p className="p-m2">{property.Interior}</p>
+              <p className="p-m2">{property.Bedrooms}</p>
+              <p className="p-m2">{property.Status}</p>
+            </div>
+          </div>
+          <hr className="hr"></hr>
+          <p className="description">{property.Discription}</p>
+          <button className="btn" onClick={() => history.goBack()}>
+            Back
+          </button>
         </div>
       </div>
-      <hr className="hr"></hr>
-      <div className="p-1">
-        <div className="p-2-1">
-          <p className="p-m1">Year Build</p>
-          <p className="p-m1">Interior</p>
-          <p className="p-m1">Bedrooms</p>
-          <p className="p-m1">Status</p>
-        </div>
-        <div className="p-2-2">
-          <p className="p-m2">{property.YearBuild}</p>
-          <p className="p-m2">{property.Interior}</p>
-          <p className="p-m2">{property.Bedrooms}</p>
-          <p className="p-m2">{property.Status}</p>
-        </div>
-      </div>
-      <hr className="hr"></hr>
-      <p className="description">{property.Discription}</p>
-      <button className="btn" onClick={() => history.goBack()}>
-        Back
-      </button>
-    </div>
-  );
+      : 
+      "Loading"
+    );
 }
+
