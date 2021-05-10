41a5b1c8a4a7dab92a8bb6ec254649c037e5b2f2
joerghebestreit <joerghebestreit@gmail.com>
2021-05-10 17:02:32 +0200

    GL installed

--- client/src/components/ContactPage.js
+++ client/src/components/ContactPage.js
@@ -1,18 +1,30 @@
 import "../components/ContactPage.css";
+import {useState} from "react"
 import {FaPhoneAlt} from "react-icons/fa"
 import {HiOutlineMail} from "react-icons/hi"
 import {GoogleMap, withScriptjs, withGoogleMap, Marker} from "react-google-maps"
+import ReactMapGL from "react-map-gl"
 import Form from "./Form"
 
 function Map() {
+  const [viewport, setViewport] = useState({
+    latitude: 52.526846,
+    longitude: 13.338450,
+    zoom: 14,
+    width: '345px',
+    heigth: '200px',
+  });
   return ( 
-    <GoogleMap defaultZoom={14} defaultCenter={{lat: 52.526846, lng: 13.338450}}>
-      <Marker position={{lat: 52.526846, lng: 13.338450}}/>
-    </GoogleMap>
+    <div>
+      <ReactMapGL {...viewport} mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}></ReactMapGL>
+      {/* <GoogleMap defaultZoom={14} defaultCenter={{lat: 52.526846, lng: 13.338450}}>
+        <Marker position={{lat: 52.526846, lng: 13.338450}}/>
+      </GoogleMap> */}
+    </div>
   )
 }
 
-const WrappedMap = withScriptjs(withGoogleMap(Map));
+// const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 function ContactPage() {
   return (
@@ -38,12 +50,13 @@ function ContactPage() {
       </div>
       <hr className="hrMap"></hr>
       <div className="mapDiv">
-        <WrappedMap
+        <Map />
+        {/* <WrappedMap
           googleMapURL={"https://maps.googleapis.com/maps/api/js?key=AIzaSyAP5KqI2BKvNTDtgTo5dbiS3F34_UvF61M&v=3.exp&libraries=geometry,drawing,places"}
           loadingElement={<div style={{ height: `200px`, width: `345px` }}/>}
           containerElement={<div style={{ height: `200px`, width: `345px` }}/>}
           mapElement={<div style={{ height: `200px`, width: `345px` }}/>}
-        />
+        /> */}
       </div>
       <hr className="hrForm"></hr>
       <Form/>
