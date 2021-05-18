47c3d7cbaa06384b13d005d2321fbd21071e7ed2
joerghebestreit <joerghebestreit@gmail.com>
2021-05-18 20:01:45 +0200

    login styling

--- client/src/components/ContactPage.js
+++ client/src/components/ContactPage.js
@@ -6,32 +6,34 @@ import Map from "./Map"
 
 function ContactPage(setViewport) {
   return (
-    <div className="textContainerContactPage">
-      <h4>Contact</h4>
-      <br></br>
-      <br></br>
-      <p>HEBESTREIT IMMOBILIEN</p>
-      <br></br>
-      <p>Mathilde-Jacob-Platz 1</p>
-      <br></br>
-      <p>10711 Berlin</p>
-      <br></br>
-      <br></br>
-      <div className="phoneIconNumber">
-        <FaPhoneAlt className="svgPhone"/>
-        <p className="phoneNumber">030 / 5 44 00 57</p>
+    <div className="contact-content">
+      <div className="textContainerContactPage">
+        <h4>Contact</h4>
+        <br></br>
+        <br></br>
+        <p>HEBESTREIT IMMOBILIEN</p>
+        <br></br>
+        <p>Mathilde-Jacob-Platz 1</p>
+        <br></br>
+        <p>10711 Berlin</p>
+        <br></br>
+        <br></br>
+        <div className="phoneIconNumber">
+          <FaPhoneAlt className="svgPhone"/>
+          <p className="phoneNumber">030 / 5 44 00 57</p>
+        </div>
+        <br></br>
+        <div className="phoneIconNumber">
+          <HiOutlineMail className="svgMail"/>
+          <p className="mailAdress">info@hebestreit-immobilien.com</p>
+        </div>
+        <hr className="hrMap"></hr>
+        <div className="mapDiv">
+          <Map setViewport={setViewport}/>
+        </div>
+        <hr className="hrForm"></hr>
+        <Form/>
       </div>
-      <br></br>
-      <div className="phoneIconNumber">
-        <HiOutlineMail className="svgMail"/>
-        <p className="mailAdress">info@hebestreit-immobilien.com</p>
-      </div>
-      <hr className="hrMap"></hr>
-      <div className="mapDiv">
-        <Map setViewport={setViewport}/>
-      </div>
-      <hr className="hrForm"></hr>
-      <Form/>
     </div>
   );
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,3 +1,5 @@
+import "./Home.css"
+
 function HomePage() {
     return (
       <div className="home">
