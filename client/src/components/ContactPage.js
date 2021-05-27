a1d118de1655da21c27494078f5ddb96c3d372fd
joerghebestreit <joerghebestreit@gmail.com>
2021-05-27 17:41:18 +0200

    changes

--- client/src/components/ContactPage.js
+++ client/src/components/ContactPage.js
@@ -1,8 +1,8 @@
 import "../components/ContactPage.css";
-import {FaPhoneAlt} from "react-icons/fa"
-import {HiOutlineMail} from "react-icons/hi"
-import Form from "./Form"
-import Map from "./Map"
+import { FaPhoneAlt } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import Form from "./Form";
+import Map from "./Map";
 
 function ContactPage(setViewport) {
   return (
@@ -19,20 +19,20 @@ function ContactPage(setViewport) {
         <br></br>
         <br></br>
         <div className="phoneIconNumber">
-          <FaPhoneAlt className="svgPhone"/>
-          <p className="phoneNumber">030 / 5 44 00 57</p>
+          <FaPhoneAlt className="svgPhone" />
+          <p className="phoneNumber">030 / 5 44 00 59</p>
         </div>
         <br></br>
         <div className="phoneIconNumber">
-          <HiOutlineMail className="svgMail"/>
+          <HiOutlineMail className="svgMail" />
           <p className="mailAdress">info@hebestreit-immobilien.com</p>
         </div>
         <hr className="hrMap"></hr>
         <div className="mapDiv">
-          <Map setViewport={setViewport}/>
+          <Map setViewport={setViewport} />
         </div>
         <hr className="hrForm"></hr>
-        <Form/>
+        <Form />
       </div>
     </div>
   );
