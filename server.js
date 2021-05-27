717f9488efe67739d05283bafad51890c6e659b5
joerghebestreit <joerghebestreit@gmail.com>
2021-05-27 12:00:28 +0200

    enable proxy for server side

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
 const express = require("express");
-const cors = require("cors");
 const app = express();
 const Apartments = require("./apartment");
 
-app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
   const { method, url } = req;
@@ -13,59 +11,60 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/properties", (req, res) => {
+app.get("/api/properties", (req, res) => {
   Apartments.find().then((apartments) => {
     res.json(apartments);
     res.status(200);
   });
 });
 
-app.get(`/properties/:id`, (req, res) => {
-  Apartments.find({ ID: req.params.id})
-  .then((apartments) => {
+app.get(`/api/properties/:id`, (req, res) => {
+  Apartments.find({ ID: req.params.id }).then((apartments) => {
     res.json(apartments);
     res.status(200);
   });
 });
 
-app.post("/properties", (req, res) => {
+app.post("/api/properties", (req, res) => {
   Apartments.create(req.body)
-  .then(newApartment => {
-    res.status(200);
-    res.json(newApartment);
-  })
-  .catch(error =>{
-    res.status(500);
-    res.json({
-      error:"internal server error",
-    })
-  })
-})
-
-app.delete("/properties/:id", (req, res) =>{
-  Apartments.findOneAndDelete({ ID: req.params.id})
-  .then((Apartments) => {
-    res.status(200);
-    res.json(Apartments);
-  })
-  .catch((error)=>{
-      console.error(error);
-  });
-});
-
-app.patch("/properties/:id", (req, res)=>{
-  Apartments.findOneAndUpdate({ID: req.params.id}, {new: true}, req.body)
-  .then((newApartment)=>{
-    if(newApartment){
+    .then((newApartment) => {
       res.status(200);
       res.json(newApartment);
-    }else{
+    })
+    .catch((error) => {
+      res.status(500);
+      res.json({
+        error: "internal server error",
+      });
+    });
+});
+
+app.delete("/api/properties/:id", (req, res) => {
+  Apartments.findOneAndDelete({ ID: req.params.id })
+    .then((Apartments) => {
+      res.status(200);
+      res.json(Apartments);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
+app.patch("/api/properties/:id", (req, res) => {
+  Apartments.findOneAndUpdate(
+    { ID: req.params.id },
+    { new: true },
+    req.body
+  ).then((newApartment) => {
+    if (newApartment) {
+      res.status(200);
+      res.json(newApartment);
+    } else {
       console.error("not found");
       res.status(404);
     }
-  })
-})
-
+  });
+});
 
 mongoose.connect(
   "mongodb+srv://joerghebestreit:yDsBhf74U94pMT32@blog-app.ovjjd.mongodb.net/capstone-real-estate?retryWrites=true&w=majority",
