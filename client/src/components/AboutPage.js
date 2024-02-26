2bb3c7e58e993c9f65e233f53487fadba451653e
Jörg Hebestreit <joerghebestreit@gmail.com>
2024-02-26 15:03:39 +0100

    delete transform on about page

--- client/src/components/AboutPage.js
+++ client/src/components/AboutPage.js
@@ -6,7 +6,7 @@ function AboutPage() {
           <div className="textContainer">
             <h4>About us</h4><br></br>
             <p className="text">The Hebestreit Group is a professional real estate brokerage serving buyers and sellers of luxury property in Germany.</p><br></br>
-            <p className="text">The brokerage is comprised of a close group of talented realtors, led by the firm’s President and Founder, Jörg Hebestreit. With more than €1B+ in total sales and €300 million in active listings, the firm closes over one hundred deals annually and has brokered many of the country’s largest residential sales, including numerous record-breaking transactions.</p>
+            <p className="text">The brokerage is comprised of a close group of talented realtors, led by the firms President and Founder, Jörg Hebestreit. With more than €1B+ in total sales and €300 million in active listings, the firm closes over one hundred deals annually and has brokered many of the countrys largest residential sales, including numerous record-breaking transactions.</p>
           </div>
         </div>
   ); 
